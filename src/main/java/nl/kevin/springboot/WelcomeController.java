cfb00ef2905b7ac7edbafa95a9c26cbaa403ca41
Kevin vdBosch <kevinvdbosch_1@hotmail.com>
2018-03-28 21:34:57 +0200

    add default example route to welcome page

--- src/main/java/nl/kevin/springboot/WelcomeController.java
+++ src/main/java/nl/kevin/springboot/WelcomeController.java
@@ -1,4 +1,13 @@
 package nl.kevin.springboot;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class WelcomeController {
+
+    @RequestMapping("/welcome")
+    public String loginMessage() {
+        return "welcome";
+    }
 }
